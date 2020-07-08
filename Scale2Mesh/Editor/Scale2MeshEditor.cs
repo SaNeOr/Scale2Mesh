0f9673809c444c1cbeb77239ca73f43497b8e28b
SaNeOr <534369503@qq.com>
2020-07-08 16:41:23 +0800

    Add files via upload

---
+++ Scale2Mesh/Editor/Scale2MeshEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Scale2Mesh))]
+public class Scale2MeshEditor : Editor
+{
+    bool isFirstMark = true;
+    public override void OnInspectorGUI()
+    {
+        Scale2Mesh s2m = target as Scale2Mesh;
+        DrawDefaultInspector();
+
+        if(isFirstMark){
+            s2m.MarkVerticesInfo();
+            isFirstMark = false;
+        }
+        if (GUILayout.Button("Scale2MeshSize")){
+            s2m.S2M();
+        }
+        if (GUILayout.Button("Change Mesh Size")){
+            s2m.ChangeMeshSize();
+        }
+
+
+    }
+}
